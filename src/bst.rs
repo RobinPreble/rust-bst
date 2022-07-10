a1ace77fca6ed36830016c6d63136602b1e12cc1
RobinPreble <robinpreble@LAPTOP-TVOTQ7HI.localdomain>
2022-07-10 11:37:56 -0700

    fixed bug where no matter what add was adding to left

--- src/bst.rs
+++ src/bst.rs
@@ -15,38 +15,47 @@ impl BST<i32> {
         BST { root: None }
     }
 
+    // add a new node to the tree
     pub fn add(&mut self, val: i32) { 
-        let new_node = Box::new(Node {
+        let new_node = Box::new(Node { // initialize node
             val: val, 
             left: None,
             right: None,
         });
-        Self::place_node(&mut self.root, new_node);
+        Self::place_node(&mut self.root, new_node); // place node in proper spot on tree
     }
-    
-    fn place_node(root: &mut Link<i32>, new_node: Box<Node<i32>>) {
-        if root.is_none() {
-            *root = Some(new_node);
-        } else if root.as_ref().unwrap().val > new_node.val {
-            Self::place_node(&mut root.as_mut().unwrap().left, new_node);
-        } else if root.as_ref().unwrap().val < new_node.val {
-            Self::place_node(&mut root.as_mut().unwrap().left, new_node);
+
+    /* private method to figure out the proper spot for the given node n on a tree
+    rooted at r and then put it there */
+    fn place_node(r: &mut Link<i32>, n: Box<Node<i32>>) {
+        if r.is_none() { 
+            *r = Some(n);
+        } else if n.val < r.as_ref().unwrap().val { 
+            Self::place_node(&mut r.as_mut().unwrap().left, n);
+        } else if n.val > r.as_ref().unwrap().val {
+            Self::place_node(&mut r.as_mut().unwrap().right, n);
         }
     }
     
-
+    /* returns a string containing the nodes in self as given by an in order traversal */
     pub fn to_string(&self) -> String {
+        println!("left: {}", &self.root.as_ref().unwrap().left.as_ref().unwrap().val);
+        println!("root: {}", &self.root.as_ref().unwrap().val);
+        println!("right: {}", &self.root.as_ref().unwrap().right.as_ref().unwrap().val);
         (*Self::in_order(&self.root, String::new())).to_string()
     }
 
-    fn in_order(root: &Link<i32>, mut s: String) -> String {
-        if root.is_none() {
+    /* private function that takes a node r and a string s containing the nodes visited so 
+    far. Performs an in order traversal on the tree rooted at r and returns a string 
+    containing the nodes in the order they are visited */
+    fn in_order(r: &Link<i32>, mut s: String) -> String {
+        if r.is_none() {
             println!("is none: {}", s);
             String::new()
         } else {
-            s = format!("{} {} ", s, Self::in_order(&root.as_ref().unwrap().left, s.clone()));
-            s = format!("{} {} ", s, root.as_ref().unwrap().val);
-            s = format!("{} {} ", s, Self::in_order(&root.as_ref().unwrap().right, s.clone()));
+            s = format!("{} {} ", s, Self::in_order(&r.as_ref().unwrap().left, s.clone()));
+            s = format!("{} {} ", s, r.as_ref().unwrap().val);
+            s = format!("{} {} ", s, Self::in_order(&r.as_ref().unwrap().right, s.clone()));
             println!("this shit: {}", s);
             s
         }
